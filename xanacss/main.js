466538f546de6f3dd51b02ccfd95f5a87c866c97
Leon Blakey <code@leonblakey.com>
2023-11-23 15:37:50 -0800

    initial egrid

--- xanacss/main.js
+++ xanacss/main.js
@@ -5,7 +5,7 @@ function offset(el) {
     return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
 }
 
-addEventListener("load", () => {
-    let val = offset(document.getElementsByClassName("unit-beacon")[2])
-    console.log("val", val)
-})
+// addEventListener("load", () => {
+//     let val = offset(document.getElementsByClassName("unit-beacon")[2])
+//     console.log("val", val)
+// })
