ff91a6c0956fb7179fd8140f6f1d22475f9a9dfc
Leon Blakey <code@leonblakey.com>
2024-04-01 13:51:22 -0700

    new history factorio-mapgen-cpu

--- maps/start-save.sh
+++ maps/start-save.sh
@@ -1,9 +1,10 @@
 factorio_root="$HOME/factorio"
 factorio_bin="$factorio_root/bin/x64/factorio"
 factorio_saves="$factorio_root/saves"
+set -x
 
 $factorio_bin \
-  --start-server "loop-miner-chunk1000v2" \
+  --start-server "lm-artful-hyperscan" \
   --rcon-port 28016 \
   --rcon-password xana \
   --server-settings "server-settings.json" \
