5e39c021dde3c00524c7e71bb8ffa20287eab1b0
Leon Blakey <code@leonblakey.com>
2025-07-08 16:52:54 -0700

    remove jemalloc

--- core/src/bin/main.rs
+++ core/src/bin/main.rs
@@ -1,6 +1,5 @@
 // Fix simd-json eating all my ram
 #[global_allocator]
-// static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 fn main() {
