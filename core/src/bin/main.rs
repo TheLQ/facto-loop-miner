b20dfdda79e2fae62201907812b0081107985bd4
Leon Blakey <code@leonblakey.com>
2025-07-11 22:28:48 -0700

    core: add malloc experiments

--- core/src/bin/main.rs
+++ core/src/bin/main.rs
@@ -1,6 +1,7 @@
 // Fix simd-json eating all my ram
-#[global_allocator]
-static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+// #[global_allocator]
+// static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+// static GLOBAL: tcmalloc_better::TCMalloc = tcmalloc_better::TCMalloc;
 
 fn main() {
     facto_loop_miner::inner_main();
