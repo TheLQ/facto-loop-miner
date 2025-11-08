b4bfaca2da48b53286af0a84006e7c37aa1c0951
Leon Blakey <code@leonblakey.com>
2025-11-07 18:27:12 -0800

    migrate to commons logger

--- common/src/logger.rs
+++ common/src/logger.rs
@@ -1,32 +1,34 @@
-use std::cell::LazyCell;
-use std::iter::IntoIterator;
-use xana_commons_rs::XanaCommonsLogConfig;
+use xana_commons_rs::{MapHugeCrateName, XanaCommonsLogConfig};
 
 const TRACE_NO_ADMIRAL_NETWORK: &str = "trace,\
 facto_loop_miner_fac_engine::admiral::executor::client=debug,\
 facto_loop_miner_fac_engine::admiral::lua_command::lua_batch=debug,\
 facto_loop_miner_fac_engine::game_blocks::rail_hope_single=debug";
 
-const FILTER_NON_MAIN_THREADS: bool = true;
-
 pub fn log_init_trace() {
-    xana_commons_rs::log_init_trace(log_config())
+    xana_commons_rs::log_init_trace(log_config(Some(TRACE_NO_ADMIRAL_NETWORK)))
 }
 
 pub fn log_init_debug() {
-    xana_commons_rs::log_init_debug(log_config())
+    xana_commons_rs::log_init_debug(log_config(None))
 }
 
-fn log_config() -> XanaCommonsLogConfig {
+fn log_config(extra_filter_env: Option<&'static str>) -> XanaCommonsLogConfig<FactoLogConfig> {
     XanaCommonsLogConfig {
-        map_huge_crate_names: [
-            ("facto_loop_miner", "core"),
-            ("facto_loop_miner_io", "io"),
-            ("facto_loop_miner_fac_engine", "engine"),
-        ]
-        .into_iter()
-        .collect(),
-        filter_non_main_threads: false,
-        extra_filter_env: "",
+        map_huge_crate_names: Some(FactoLogConfig),
+        filter_non_main_threads: true,
+        extra_filter_env: extra_filter_env.unwrap_or(""),
+    }
+}
+
+struct FactoLogConfig;
+impl MapHugeCrateName for FactoLogConfig {
+    fn map_huge(&self, input: &str) -> Option<&'static str> {
+        match input {
+            "facto_loop_miner" => Some("core"),
+            "facto_loop_miner_io" => Some("io"),
+            "facto_loop_miner_fac_engine" => Some("engine"),
+            _ => None,
+        }
     }
 }
