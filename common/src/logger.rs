df9ed25b199b00621a585dc355ceebd3a0f30710
Leon Blakey <code@leonblakey.com>
2026-04-17 06:52:37 -0700

    core: update logger api

--- common/src/logger.rs
+++ common/src/logger.rs
@@ -1,19 +1,20 @@
 use xana_commons_rs::{MapHugeCrateName, XanaCommonsLogConfig};
 
-const TRACE_NO_ADMIRAL_NETWORK: &str = "trace,\
-facto_loop_miner_fac_engine::admiral::executor::client=debug,\
-facto_loop_miner_fac_engine::admiral::lua_command::lua_batch=debug,\
-facto_loop_miner_fac_engine::game_blocks::rail_hope_single=debug";
+const TRACE_NO_ADMIRAL_NETWORK: [&str; 3] = [
+    "facto_loop_miner_fac_engine::admiral::executor::client=debug",
+    "facto_loop_miner_fac_engine::admiral::lua_command::lua_batch=debug",
+    "facto_loop_miner_fac_engine::game_blocks::rail_hope_single=debug",
+];
 
 pub fn log_init_trace() {
-    log_config(TRACE_NO_ADMIRAL_NETWORK).log_init_trace()
+    log_config(&[]).log_init_trace()
 }
 
 pub fn log_init_debug() {
-    log_config("").log_init_debug()
+    log_config(&[]).log_init_debug()
 }
 
-fn log_config(extra_filter_env: &'static str) -> XanaCommonsLogConfig<FactoLogConfig> {
+fn log_config(extra_filter_env: &'static [&'static str]) -> XanaCommonsLogConfig<FactoLogConfig> {
     XanaCommonsLogConfig::new_map_huge()
         .with_extra_filter_env(extra_filter_env)
         .with_filter_non_main_threads(true)
